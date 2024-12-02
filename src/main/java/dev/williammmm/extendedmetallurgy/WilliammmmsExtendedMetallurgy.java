edb5ee0ba56969f17a6560d38afe0b2ec25ea0f1
williammmm <91826947+williamwith4ms@users.noreply.github.com>
2024-12-02 19:53:07 +0000

    initial  commit

---
+++ src/main/java/dev/williammmm/extendedmetallurgy/WilliammmmsExtendedMetallurgy.java
@@ -0,0 +1,15 @@
+package dev.williammmm.extendedmetallurgy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WilliammmmsExtendedMetallurgy implements ModInitializer {
+	public static final String MOD_ID = "williammmms-extended-metallurgy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/dev/williammmm/extendedmetallurgy/WilliammmmsExtendedMetallurgyClient.java
@@ -0,0 +1,10 @@
+package dev.williammmm.extendedmetallurgy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WilliammmmsExtendedMetallurgyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
